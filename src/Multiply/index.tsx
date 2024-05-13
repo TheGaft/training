493aa78aaa2c5f122c7b500831c26dc28292b803
garloy <n.sannikov@netrika.ru>
2024-05-13 17:32:16 +0300

    stack generation

--- src/Multiply/index.tsx
+++ src/Multiply/index.tsx
@@ -1,5 +1,5 @@
-import { random, shuffle } from 'lodash';
-import { DOMAttributes, useReducer, useRef, useState } from 'react';
+import { flatMap, flatten, isEmpty, range, shuffle } from 'lodash';
+import { DOMAttributes, useEffect, useReducer, useRef, useState } from 'react';
 import { Timer } from '../components/Timer';
 import { flushSync } from 'react-dom';
 import styles from './index.module.scss';
@@ -21,19 +21,25 @@ type Action =
     | {
           type: 'SET_MAX';
           payload: number;
+      }
+    | {
+          type: 'SET_STACK';
+          payload: (readonly [number, number])[];
       };
 
 type State = {
-    numbers: [number, number];
+    numbers: readonly [number, number];
     maximum: number;
     result: number;
     errors: number;
     run: boolean;
     end: boolean;
+    stack?: (readonly [number, number])[];
+    message?: string;
 };
 
 const initial: State = {
-    numbers: [1, 1],
+    numbers: [1, 1] as const,
     maximum: 5,
     result: -1,
     errors: 0,
@@ -41,31 +47,65 @@ const initial: State = {
     end: false,
 };
 
+const getStack = (minimum: number, maximum: number) => {
+    const newStack = flatMap(range(minimum, maximum + 1), (n1) =>
+        range(2, 10).map((n2) => [n1, n2] as const),
+    );
+
+    return flatten([shuffle(newStack), shuffle(newStack)]);
+};
+
 const reducer = (state: State, action: Action): State => {
     switch (action.type) {
         case 'NEXT': {
-            console.log(state);
             if (state.end) {
                 return state;
             }
+            const [numbers, ...stack] = state.stack ?? [];
+            if (isEmpty(state.stack)) {
+                return {
+                    ...state,
+                    numbers: [1, 1],
+                    end: true,
+                    result: state.result + 1,
+                    message: 'Все примеры решены',
+                };
+            }
             return {
                 ...state,
-                numbers: shuffle([random(2, state.maximum), random(1, 9)]) as [
-                    number,
-                    number,
-                ],
+                numbers: shuffle(numbers) as [number, number],
+                stack,
                 result: state.result + 1,
                 run: true,
+            };
+        }
+        case 'SET_STACK': {
+            return {
+                ...state,
+                stack: action.payload,
+            };
+        }
+        case 'SET_MAX': {
+            return {
+                ...state,
+                maximum: action.payload,
             };
         }
         case 'RESET': {
             return initial;
         }
         case 'END': {
-            return { ...state, end: true };
+            return {
+                ...state,
+                end: true,
+                message: 'Время истекло',
+            };
         }
         case 'ERROR': {
-            return { ...state, errors: state.errors + 1 };
+            return {
+                ...state,
+                errors: state.errors + 1,
+            };
         }
         default: {
             return state;
@@ -74,7 +114,7 @@ const reducer = (state: State, action: Action): State => {
 };
 
 export const Multiply = () => {
-    const [{ numbers, maximum, result, run, end, errors }, dispatch] =
+    const [{ numbers, maximum, result, run, end, errors, message }, dispatch] =
         useReducer(reducer, initial);
 
     const [minutes, setMinutes] = useState(2);
@@ -84,6 +124,16 @@ export const Multiply = () => {
     const onReset = () => dispatch({ type: 'RESET' });
     const onError = () => dispatch({ type: 'ERROR' });
     const onNext = () => dispatch({ type: 'NEXT' });
+    const onSetStack = (payload: (readonly [number, number])[]) =>
+        dispatch({ type: 'SET_STACK', payload });
+    const onSetMax = (payload: number) =>
+        dispatch({ type: 'SET_MAX', payload });
+
+    useEffect(() => {
+        if (!run) {
+            onSetStack(getStack(2, maximum));
+        }
+    }, [run, maximum]);
 
     const testAnswer: DOMAttributes<HTMLFormElement>['onSubmit'] = (e) => {
         e.preventDefault();
@@ -98,8 +148,10 @@ export const Multiply = () => {
 
     return (
         <form onSubmit={testAnswer}>
+            {message && <h2>{message}</h2>}
             {!run && (
-                <>
+                <div>
+                    <div>Выбрать время:</div>
                     {Array.from({ length: 10 }).map((_, i) => (
                         <button
                             type="button"
@@ -112,7 +164,24 @@ export const Multiply = () => {
                             {i + 1}
                         </button>
                     ))}
-                </>
+                </div>
+            )}
+            {!run && (
+                <div>
+                    <div>Выбрать максимальный множитель:</div>
+                    {Array.from({ length: 8 }).map((_, i) => (
+                        <button
+                            type="button"
+                            key={i}
+                            onClick={() => onSetMax(i + 2)}
+                            className={classNames(styles.btn, {
+                                [styles.activeBtn]: maximum === i + 2,
+                            })}
+                        >
+                            {i + 2}
+                        </button>
+                    ))}
+                </div>
             )}
 
             {run && !end && <Timer minutes={minutes} options={{ onEnd }} />}
@@ -129,6 +198,7 @@ export const Multiply = () => {
                     maxLength={2}
                     style={{ width: '10ch' }}
                     ref={inp}
+                    disabled={end}
                 />
                 <button type="reset">X</button>
             </div>
