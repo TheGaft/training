249287b373d279b486918e9d91b0150a355fe62c
garloy <n.sannikov@netrika.ru>
2024-05-13 16:07:17 +0300

    classnames lodash rooks

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [react()],
+        plugins: [react(), optimizeLodashImports()],
+        server: {
+            host: '0.0.0.0',
+            port: parseInt(env.FRONT_PORT || '3000', 10),
+            hmr: {
+                host: '0.0.0.0',
+                port: parseInt(env.FRONT_PORT_HMR || '3010', 10),
+            },
+        },
+        preview: {
+            host: '0.0.0.0',
+            port: parseInt(env.FRONT_PORT || '3000', 10),
+        },
         base: env.STATIC_URL || '/training',
     };
 });
